9528478809818343b24e8d6be852cabd890aa266
zendrias <zenas@live.com>
2022-05-07 21:55:08 -0400

    stub out form and controlled input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { useState, useReducer } from 'react'
+
+
+function reducer() {
+
+}
 
 function App() {
+  const [name, setName] = useState('')
+  const [todos, dispatch] = useReducer(reducer, [])
+
   return (
     <div className="App">
-     <h1>Zena's To-do List</h1>
+      <h1>Zena's To-do List</h1>
+      <form>
+        <input type="text" onChange={(e) => setName(e.target.value)} value={name} />
+        <button type="submit">Add To-do</button>
+      </form>
     </div>
   );
 }

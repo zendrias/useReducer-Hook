6881e115ce5b71b3b5bea107ab43d2a48c16b615
zendrias <zenas@live.com>
2022-05-07 22:04:29 -0400

    write reducer and handle submit functions

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import './App.css';
 import { useState, useReducer } from 'react'
 
+const ACTIONS = {
+  ADD_TODO: 'add_todo'
+}
 
-function reducer() {
-
+function reducer(todos, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, action.payload]
+    default:
+      return todos
+  }
 }
 
 function App() {
   const [name, setName] = useState('')
   const [todos, dispatch] = useReducer(reducer, [])
 
+  function handleSubmit(e) {
+    e.preventDefault()
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name, id: Date.now, complete: false } })
+    setName('')
+  }
+
   return (
     <div className="App">
       <h1>Zena's To-do List</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" onChange={(e) => setName(e.target.value)} value={name} />
         <button type="submit">Add To-do</button>
       </form>
